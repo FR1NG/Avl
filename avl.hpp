a1cc03cc9fc60533ef2c0b51b58e269b1e26aa5b
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-02-09 18:49:36 +0100

    backup

--- avl.hpp
+++ avl.hpp
@@ -1,17 +1,23 @@
 #pragma once
+#include <functional>
 #include <iostream>
 
-template <class Type> class Avl {
+template <class Type, class Comp> class Avl {
 public:
   typedef Type value_type;
   typedef value_type *pointer;
   typedef value_type &reference;
+  typedef Comp compare;
+private:
+  compare compare_;
+
   // Node class [ begin ]
   class Node {
   private:
     Type *data_;
     Node *left_;
     Node *right_;
+    compare compare_;
 
   public:
     typedef Type value_type;
@@ -24,6 +30,11 @@ public:
     Node *getRight() const { return this->right_; }
     void setLeft(const Node *node) { this->right_ = node; }
     void setRigh(const Node *node) { this->right_ = node; }
+    pointer getData() const { return this->data_;}
+
+    bool operator<(const Node &node) {
+      return compare_(this->key(), node.key());
+    }
     ~Node() { delete data_; }
   };
   // node class [ end ]
@@ -34,4 +45,13 @@ public:
   public:
   Avl(): root_(NULL) {}
   Avl(const value_type data): root_(new Node(data)) {}
+  Node* insert(const value_type data)
+  {
+   Node* head; 
+    head = this->root_;
+    while(head)
+    {
+        if (compare_(k))
+      }
+  }
 };
