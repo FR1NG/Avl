dd98b7b150783ea75ecc537001df029edd0ba64e
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2023-02-12 15:57:59 +0100

    added print edges for tree visualisation

---
+++ dev/helpers.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+namespace ft {
+void printEdges(int height, int level, int maxHeight);
+std::string repeat(std::string str, int n);
+} // namespace ft
